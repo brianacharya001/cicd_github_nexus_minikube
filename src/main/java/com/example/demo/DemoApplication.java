44ace2c848257b9d88b726a43dc408b72cc12e36
samsharma777 <samsharma77@gmail.com>
2026-01-11 08:04:43 -0500

    update spring boot application with Dockerfile and Helm chart

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,16 @@
-package main.java.com.example.demo;
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
 public class DemoApplication {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/health")
+    public String health() { return "Spring Boot is Running on Minikube!"; }
 }
